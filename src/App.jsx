56dff2ceae78b9dc269a4d12b3fcd865d24b0fff
PedroHenriqueLOURENCO <rm98402@fiap.com.br>
2023-08-18 20:14:31 -0300

    Alteração realizada no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React Coded by RM98402</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
